b98e2bad112f6e88f014b6ae9e4a7df60048b659
WildBoarGonnaGo <unknownthrill814@gmail.com>
2021-09-24 00:41:00 +0300

    widget redraw is still failing

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -3,6 +3,7 @@
 #include <numsyscalc.h>
 #include <calculator.hpp>
 #include <QApplication>
+#include <QVBoxLayout>
 
 class MainWindow : public QWidget {
     Q_OBJECT
@@ -17,6 +18,7 @@ private:
     QApplication        *_mainApp;
 	QAction             *_calcSimpleCheckPtr;
 	QAction             *_calcNumSysCheckPtr;
+	QVBoxLayout         *_tmpPtr;
 private slots:
     void                menuCheckLinesVerify();
 };
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,6 @@
 ﻿#include <mainwindow.h>
 #include <QMenu>
 #include <QMenuBar>
-#include <QVBoxLayout>
 
 MainWindow::MainWindow(QWidget *wgtptr) : QWidget(wgtptr), _secWgtPtr(nullptr),
     _sysNumWgtPtr(new numSysCalcWgt()), _calcWgtPtr(new Calculator()) {
@@ -18,7 +17,7 @@ MainWindow::MainWindow(QWidget *wgtptr) : QWidget(wgtptr), _secWgtPtr(nullptr),
     _calcNumSysCheckPtr->setChecked(true);
 
     itemCheckMenu->addSeparator();
-    itemCheckMenu->addAction("&Exit", _mainApp, SLOT(quit()));
+    itemCheckMenu->addAction("&Exit", this, SLOT(quit()));
 
     mbar->addMenu(itemCheckMenu);
     _secWgtPtr = _sysNumWgtPtr;
@@ -27,9 +26,10 @@ MainWindow::MainWindow(QWidget *wgtptr) : QWidget(wgtptr), _secWgtPtr(nullptr),
     vbox->addWidget(_secWgtPtr);
     vbox->setContentsMargins(0, 0, 0, 0);
     vbox->setSpacing(0);
+	_tmpPtr = vbox;
 
-    connect(_calcSimpleCheckPtr, SIGNAL(toggled(bool)), this, SLOT(menuCheckLinesVerify()));
-    connect(_calcNumSysCheckPtr, SIGNAL(toggled(bool)), this, SLOT(menuCheckLinesVerify()));
+    connect(_calcSimpleCheckPtr, SIGNAL(triggered(bool)), this, SLOT(menuCheckLinesVerify()));
+    connect(_calcNumSysCheckPtr, SIGNAL(triggered(bool)), this, SLOT(menuCheckLinesVerify()));
 
     setWindowTitle("Calculator");
     setLayout(vbox);
@@ -45,11 +45,18 @@ QApplication const  &MainWindow::getQApplication() const {
 }
 
 void                MainWindow::menuCheckLinesVerify() {
-	if (_calcSimpleCheckPtr->isChecked()) {
-		_secWgtPtr = _calcWgtPtr;
-		_calcNumSysCheckPtr->setChecked(false);
-	} else {
+	QObject *rage = sender();
+
+	if (_calcSimpleCheckPtr->isChecked() && rage == _calcNumSysCheckPtr) {
 		_secWgtPtr = _sysNumWgtPtr;
 		_calcSimpleCheckPtr->setChecked(false);
+		_calcNumSysCheckPtr->setChecked(true);
 	}
+	else if (_calcNumSysCheckPtr->isChecked() && rage == _calcSimpleCheckPtr) {
+		_secWgtPtr = _calcWgtPtr;
+		_calcNumSysCheckPtr->setChecked(false);
+		_calcSimpleCheckPtr->setChecked(true);
+	}
+	_tmpPtr->update();
+	update();
 }
